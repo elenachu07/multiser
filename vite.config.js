5463aa5567b9bc8699c46c0e1b1db4708a592e76
Elena <elena@MacBook-Pro.local>
2026-01-06 17:24:43 +0100

    changes

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react-swc"
+import path from "path"
 
-
-export default defineConfig ({
-  pluggins: [react()],
-  base: "/multiser"
+export default defineConfig({
+  plugins: [react()],
+  base: "/multiser/",
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
 })
